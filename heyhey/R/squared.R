f3ece7c623a166f432167c9006ce761538e54971
jackmat <sansguitar@hotmail.com>
2016-10-07 15:55:26 +0200

    coses provades

---
+++ heyhey/R/squared.R
@@ -0,0 +1,22 @@
+#' Squared function
+#'
+#' @author Carles Sans
+#'
+#' @description  You input as a value a numeric number or vector and it gives back the squared number
+#'
+#' @param x que es numeric
+#' @return x**2 basicament
+#' 
+#' @examples squared(2)
+#' @examples squared(30)
+#'
+#' 
+#' 
+#'
+#'
+#' @export
+
+squared<- function(X){
+  return(X**2)
+  
+}
